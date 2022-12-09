ebfa3fa2e1827c9798e565ddbedeb464c4da2852
mathio.dz <40936957+mathio3@users.noreply.github.com>
2022-12-09 08:21:18 -0800

    Update rsa.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -1,1 +1,17 @@
+#include "rsa.h"
+ 
+void rsa::SetPrivk()
+{
+	rsa::Pem_Priv_Key
+}
 
+void rsa::SetPubk()
+{
+	rsa::Pem_Public_Key
+}
+
+BYTE * rsa::enc() {}
+
+BYTE * rsa::dec() {}
+
+rsa::clear() {};
